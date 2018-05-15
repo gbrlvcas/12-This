dcf005e033644bf3e683823e8f3d01e61872a933
104969 <104969@SALA409.proway.treina>
2018-05-15 20:47:28 -0300

    Exemplo 12

---
+++ src/exemplo01/Calculo.java
@@ -0,0 +1,16 @@
+package exemplo01;
+
+public class Calculo {
+	
+	//Atributo da classe
+	private int numero = 5;
+	
+	//Método da classe
+	public void exibirNumero() {
+		
+		int numero = 8;
+		
+		System.out.println(this.numero);
+	}
+
+}
---
+++ src/exemplo01/Principal.java
@@ -0,0 +1,12 @@
+package exemplo01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Calculo c = new Calculo();
+		c.exibirNumero();
+
+	}
+
+}
